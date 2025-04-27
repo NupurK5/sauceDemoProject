43acada5c5665f32187cf8032cdc4443ddc25434
Nupur Kaushik <nupurkaushik5@gmail.com>
2025-04-27 19:03:49 +0530

    adding config file

--- src/test/java/com/saucedemo/testCases/Base.java
+++ src/test/java/com/saucedemo/testCases/Base.java
@@ -2,6 +2,8 @@ package com.saucedemo.testCases;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
@@ -10,15 +12,28 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class Base {
 	
 	public static WebDriver driver;
-	
+	public String url = "https://www.saucedemo.com/";
+	public String browser = "chrome";
 	
 	@BeforeSuite
-	public void launchBrowser() {
+	public void launchBrowser(String browser, String url) {
 		
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
-		driver.get("https://www.saucedemo.com/");
+		if(browser.equalsIgnoreCase("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		}
+		else if(browser.equalsIgnoreCase("Edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();			
+		}
+		else if(browser.equalsIgnoreCase("Firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		}
+		
+		driver.get(url);
 		driver.manage().window().maximize();
+		
 		
 	}
 	
